542d972af67f618250b9b77c13e47a9702904751
v79 <liamjdavison@gmail.com>
2021-08-17 07:28:44 +0100

    A lot of pointless pain over CSS

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,18 @@
 import io.kotless.plugin.gradle.dsl.kotless
-import io.terraformkt.aws.data.route53.route53_delegation_set
+import com.meiuwa.gradle.sass.SassTask
 
 plugins {
     kotlin("jvm") version "1.4.21"
     id("io.kotless") version "0.2.0-g" apply true
+    id("com.meiuwa.gradle.sass") version "2.0.0"
 }
 
 group = "org.liamjd.kotless"
 version = "0.1.0-SNAPSHOT"
+
+val ktorVersion by extra("1.5.0")
+val kotlinVersion by extra("1.4.21")
+val kotlessVersion by extra("0.2.0-f")
 
 repositories {
     jcenter()
@@ -17,19 +22,20 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("io.kotless", "ktor-lang", "0.2.0-f")
-    implementation("io.kotless", "ktor-lang-aws", "0.2.0-f")
-    implementation("io.ktor:ktor-auth:1.5.0")
-    implementation("io.ktor:ktor-auth-jwt:1.5.0")
-    implementation("io.ktor:ktor-client-core:1.5.0")
-    implementation("io.ktor:ktor-client-cio:1.5.0")
-    implementation("io.ktor:ktor-client-serialization:1.5.0")
+    implementation("io.kotless:ktor-lang:$kotlessVersion")
+    implementation("io.kotless:ktor-lang-aws:$kotlessVersion")
+    implementation("io.ktor:ktor-auth:$ktorVersion")
+    implementation("io.ktor:ktor-auth-jwt:$ktorVersion")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-html-builder:$ktorVersion")
 
     // testing
-    testImplementation("io.ktor:ktor-server-test-host:1.5.0")
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.4.21")
 
-    implementation("io.ktor:ktor-client-mock:1.5.0")
+    implementation("io.ktor:ktor-client-mock:$ktorVersion")
     testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.6.0")
 }
 
@@ -52,6 +58,10 @@ kotless {
                 region = "eu-west-2"
             }
         }
+
+        optimization  {
+            mergeLambda = io.kotless.KotlessConfig.Optimization.MergeLambda.None
+        }
     }
 
     webapp {
@@ -63,4 +73,20 @@ kotless {
             useAWSEmulation = false
         }
     }
+}
+
+application {
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=true")
+}
+
+sass {
+   executable = "sass"
+    download {
+        enabled = false
+    }
+}
+
+tasks.named<SassTask>("sassCompile") {
+    source = fileTree("src/main/resources/sass/")
+    output = file("src/main/resources/static/css")
 }
